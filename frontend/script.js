34fd7d7ef041f4fd1ccb33454e913faf1ecf6d36
KIRUBAKARAN2344 <kirubakaranmathes@gmail.com>
2026-06-09 22:06:20 +0530

    Add expense calculation logic

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,27 @@
-console.log("Expense Tracker Running");
+let income = 0;
+let totalExpense = 0;
+
+document.getElementById("income").addEventListener("change", function () {
+    income = Number(this.value);
+
+    document.getElementById("totalIncome").innerText = income;
+
+    document.getElementById("remainingBudget").innerText =
+        income - totalExpense;
+});
+
+function addExpense() {
+
+    let amount =
+        Number(document.getElementById("amount").value);
+
+    totalExpense += amount;
+
+    document.getElementById("totalExpense").innerText =
+        totalExpense;
+
+    document.getElementById("remainingBudget").innerText =
+        income - totalExpense;
+
+    document.getElementById("amount").value = "";
+}
