27e405ae03f3cb8933e4ce0088edc157e2b2a8f7
KIRUBAKARAN2344 <kirubakaranmathes@gmail.com>
2026-06-09 22:39:08 +0530

    Implement budget prediction using Linear Regression

--- models/budget_prediction.py
+++ models/budget_prediction.py
@@ -1,1 +1,13 @@
-print("Budget Prediction Model")
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+months = np.array([1, 2, 3, 4, 5, 6]).reshape(-1, 1)
+expenses = np.array([10000, 12000, 15000, 17000, 18000, 20000])
+
+model = LinearRegression()
+model.fit(months, expenses)
+
+next_month = np.array([[7]])
+prediction = model.predict(next_month)
+
+print("Predicted Expense for Next Month: ₹", int(prediction[0]))
