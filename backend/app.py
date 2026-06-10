4dcc57f365b8406be10e1cb7104ea4761e124247
KIRUBAKARAN2344 <kirubakaranmathes@gmail.com>
2026-06-10 23:22:21 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,21 @@
 from flask import Flask, jsonify, send_from_directory
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-import os
 
 app = Flask(__name__)
 
-# Home Page
 @app.route("/")
 def home():
     return send_from_directory("../frontend", "index.html")
 
-# CSS File
 @app.route("/style.css")
 def style():
     return send_from_directory("../frontend", "style.css")
 
-# JavaScript File
 @app.route("/script.js")
 def script():
     return send_from_directory("../frontend", "script.js")
 
-# AI Prediction API
 @app.route("/predict")
 def predict():
 
@@ -32,10 +27,13 @@ def predict():
     model = LinearRegression()
     model.fit(X, y)
 
-    prediction = model.predict([[11]])
+    next_month = data["Month"].max() + 1
+
+    prediction = model.predict([[next_month]])
 
     return jsonify({
-        "predicted_expense": round(float(prediction[0]), 2)
+        "predicted_expense": round(float(prediction[0]), 2),
+        "forecast_month": int(next_month)
     })
 
 if __name__ == "__main__":
