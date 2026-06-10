a53a815076df904dbbc0a528b5d7452ddc92a8e3
KIRUBAKARAN2344 <kirubakaranmathes@gmail.com>
2026-06-10 21:59:34 +0530

    Connected AI model with Flask

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,29 @@
-from flask import Flask, send_from_directory
-import os
+from flask import Flask, jsonify
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
-app = Flask(__name__,
-            static_folder="../frontend",
-            static_url_path="")
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return send_from_directory(app.static_folder, 'index.html')
+    return "Smart Expense Tracker Backend Running"
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/predict")
+def predict():
+
+    data = pd.read_csv("datasets/expenses.csv")
+
+    X = data[["Month"]]
+    y = data["Expense"]
+
+    model = LinearRegression()
+    model.fit(X, y)
+
+    prediction = model.predict([[11]])
+
+    return jsonify({
+        "predicted_expense": round(float(prediction[0]), 2)
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
